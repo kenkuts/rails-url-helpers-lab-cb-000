e7eeca6c431e77c837153072902df8a54b9ab10c
kenkuts <kennethchoi19@gmail.com>
2019-04-21 21:23:30 -0400

    review on rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Kenneth",last_name: "Kutschera")
+Student.create(first_name: "Chuck",last_name: "Taylor")
+Student.create(first_name: "Jesus",last_name: "Christ")
+Student.create(first_name: "Amon",last_name: "Armath")
